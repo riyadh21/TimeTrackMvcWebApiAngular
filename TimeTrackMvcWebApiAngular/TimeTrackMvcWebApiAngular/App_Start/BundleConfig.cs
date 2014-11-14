2272fccacdde917289e61a60092ee37cf73ac7f2
Riyadh ul islam <riyadh21@gmail.com>
2014-11-14 15:08:13 +0600

    enable font awesome and bundle configuration font awesome and angular CDN

--- TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/App_Start/BundleConfig.cs
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/App_Start/BundleConfig.cs
@@ -20,15 +20,18 @@ namespace TimeTrackMvcWebApiAngular
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                    "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    "~/Content/bootstrap.css",
+                    "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
+                    "~/Content/font-awesome.css", new CssRewriteUrlTransform()
+                    ));
 
             bundles.Add(new ScriptBundle("~/bundles/angular", AngularCdn).Include(
                      "~/Scripts/angular.js"));
