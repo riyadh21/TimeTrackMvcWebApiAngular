37d68641f9d86667b9626d663835a6d98fb00039
Riyadh ul islam <riyadh21@gmail.com>
2014-12-09 18:16:23 +0600

    user login

--- TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/controllers/loginController.js
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/controllers/loginController.js
@@ -1,3 +1,22 @@
-﻿app.controller('loginController', function ($scope) {
-    $scope.about = "Time Tracking Tools Login";
-});
+﻿app.controller('loginController', ['$scope', '$location', 'authService', function ($scope, $location, authService) {
+
+    $scope.loginData = {
+        userName: "",
+        password: ""
+    };
+
+    $scope.message = "";
+
+    $scope.login = function () {
+
+        authService.login($scope.loginData).then(function (response) {
+
+            //$location.path('/orders');
+
+        },
+         function (err) {
+             $scope.message = err.error_description;
+         });
+    };
+
+}]);
