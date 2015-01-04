7ca8dedac4243b34fb3458157b7183baa68cc8e2
Riyadh ul islam <riyadh21@gmail.com>
2015-01-05 00:39:30 +0600

    add project

--- TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/controllers/newprojectController.js
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/controllers/newprojectController.js
@@ -1,7 +1,23 @@
 ﻿app.controller('newprojectController', ['$scope', 'authService', function ($scope, authService) {
     $scope.test = "Time Tracking Tools user";
+    $scope.failMessage = "";
+    $scope.successMessage = "";
+
+    //Project data
+    $scope.newProject = {
+        projectName: '',
+        projectDescription: '',
+        projectStartDate: '',
+        projectEndDate: ''
+    };
 
     $scope.addNewProject = function () {
-        alert("");
+        if ($scope.newProject.projectName == '') {
+            $scope.failMessage = "Please fill up all the fields";
+            $scope.successMessage = '';
+        } else {
+            $scope.successMessage = "Project added successfully";
+            $scope.failMessage = "";
+        }
     };
 }]);
