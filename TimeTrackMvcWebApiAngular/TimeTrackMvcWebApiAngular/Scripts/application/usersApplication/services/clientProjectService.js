d6ca260e971a1ad0a9f7a53918ec8d6a293e33e1
Riyadh ul islam <riyadh21@gmail.com>
2015-01-12 17:37:45 +0600

    add new client service factory

---
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/services/clientProjectService.js
@@ -0,0 +1,38 @@
+﻿app.factory('clientProjectService', ['$http', '$q', 'localStorageService', 'authService', 'baseUrlService', 'ajaxRequest', function ($http, $q, localStorageService, authService, baseUrlService, ajaxRequest) {
+    var serviceBase = baseUrlService.baseUrl();
+    
+    var projectServiceFactory = {};
+
+    var _addNewClientProject = function (clientProject) {
+        var userInfo = localStorageService.getItem("authorizationData");
+        var accessToken = userInfo.token;
+        var userEmail = userInfo.userName;
+        clientProject.ClientAddedBy = userEmail;
+        
+        var requestData = {
+            url: serviceBase + 'api/ClientProject/AddClient',
+            data: clientProject,
+            token: accessToken
+        };
+        ajaxRequest.submit(requestData.url, requestData.data, requestData.token);
+    };
+
+    var _editProject = function(project) {
+        
+    };
+
+    var _loadAllProject = function() {
+        
+    };
+
+    var _deleteProject = function(project) {
+
+    };
+    
+    projectServiceFactory.addNewClientProject = _addNewClientProject;
+    projectServiceFactory.editProject = _editProject;
+    projectServiceFactory.loadAllProject = _loadAllProject;
+    projectServiceFactory.deleteProject = _deleteProject;
+
+    return projectServiceFactory;
+}]);
