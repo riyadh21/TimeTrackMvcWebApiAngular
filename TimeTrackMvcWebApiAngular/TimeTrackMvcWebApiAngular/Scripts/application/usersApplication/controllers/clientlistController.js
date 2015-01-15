e7cf387a600aace504c114b4cb2aef98cdab897b
Riyadh ul islam <riyadh21@gmail.com>
2015-01-15 17:41:46 +0600

    add angular controller for client list. load all client lists

--- TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/controllers/clientlistController.js
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/controllers/clientlistController.js
@@ -1,3 +1,25 @@
-﻿app.controller('clientlistController', function ($scope) {
+﻿app.controller('clientlistController', ['$scope', 'authService', 'projectService', 'clientProjectService', function ($scope, authService, projectService, clientProjectService) {
+    //init message
+    $scope.failMessage = "";
+    $scope.successMessage = "";
+    
+    //alert
+    $scope.closeAlert = function () {
+        $scope.failMessage = "";
+        $scope.successMessage = "";
+    };
+    
+    //$scope.clientList();
+    $scope.clientList = function () {
+        $scope.clients = {};
+        clientProjectService.loadAllClientProject().then(function (response) {
+            $scope.clients = response.data;
+        });
+    };
 
-});
+    function clients() {
+        $scope.clientList();
+    }
+
+    clients();
+}]);
