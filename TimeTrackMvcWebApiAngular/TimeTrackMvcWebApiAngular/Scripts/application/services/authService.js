4673e2429bbc6e687caab680d1e9a24fcb3906aa
Riyadh ul islam <riyadh21@gmail.com>
2015-01-30 13:37:24 +0600

    set regfresh_token in local storage.

--- TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/services/authService.js
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/services/authService.js
@@ -27,7 +27,7 @@
 
         $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
             .success(function (response) {
-                localStorageService.setItem('authorizationData', { token: response.access_token, userName: loginData.userName });
+                localStorageService.setItem('authorizationData', { refreshToken: response.refresh_token, token: response.access_token, userName: loginData.userName });
 
                 _authentication.isAuth = true;
                 _authentication.userName = loginData.userName;
