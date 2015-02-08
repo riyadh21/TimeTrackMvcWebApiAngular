e849ba771e81588ce46d1983a337d49e2cac592e
Riyadh ul islam <riyadh21@gmail.com>
2015-02-09 00:25:07 +0600

    add new service in clientservide. this service will call edit api and edit client

--- TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/services/clientProjectService.js
+++ TimeTrackMvcWebApiAngular/TimeTrackMvcWebApiAngular/Scripts/application/usersApplication/services/clientProjectService.js
@@ -28,7 +28,27 @@
     };
 
     var _editClientProject = function (clientProject) {
-        
+        var userInfo = localStorageService.getItem("authorizationData");
+        var accessToken = userInfo.token;
+        var userEmail = userInfo.userName;
+        clientProject.ClientAddedBy = userEmail;
+
+        var requestData = {
+            url: serviceBase + 'api/ClientProject/EditClient',
+            data: clientProject,
+            token: accessToken
+        };
+        ajaxRequest.submit(requestData.url, requestData.data, requestData.token).success(function (response) {
+
+        }).error(function (data, status, headers, config) {
+            if (status == '401') {
+                authService.logOut();
+                window.location.href = '/#/login';
+            }
+            else if (status == '500') {
+                //alert("Server error");
+            }
+        });
     };
 
     var _loadAllClientProject = function () {
@@ -55,6 +75,31 @@
             }
         });
     };
+    
+    var _loadClientProject = function (clientName) {
+        var userInfo = localStorageService.getItem("authorizationData");
+        var accessToken = userInfo.token;
+
+        var requestData = {
+            url: serviceBase + 'api/ClientProject/GetClient/' + clientName,
+            data: {},
+            cache: false,
+            async: false,
+            token: accessToken
+        };
+
+        return ajaxRequest.get(requestData.url, requestData.data, requestData.token).success(function (response) {
+
+        }).error(function (data, status, headers, config) {
+            if (status == '401') {
+                authService.logOut();
+                window.location.href = '/#/login';
+            }
+            else if (status == '500') {
+                //alert("Server error");
+            }
+        });
+    };
 
     var _deleteClientProject = function (clientProject) {
 
@@ -63,6 +108,7 @@
     clientProjectServiceFactory.addNewClientProject = _addNewClientProject;
     clientProjectServiceFactory.editClientProject = _editClientProject;
     clientProjectServiceFactory.loadAllClientProject = _loadAllClientProject;
+    clientProjectServiceFactory.loadClientProject = _loadClientProject;
     clientProjectServiceFactory.deleteClientProject = _deleteClientProject;
 
     return clientProjectServiceFactory;
